872959784f07ab704177d65c2753861d725981f7
nam20 <nam741585@naver.com>
2019-12-12 13:09:00 +0900

    new project

---
+++ src/main/java/com/example/nam21/Nam21Application.java
@@ -0,0 +1,13 @@
+package com.example.nam21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nam21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nam21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nam21/Nam21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nam21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nam21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
